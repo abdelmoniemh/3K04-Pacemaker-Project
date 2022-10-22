8916c9b9741627c36cfef18f07e66d188dc708d9
jinesha870 <113379258+jinesha870@users.noreply.github.com>
2022-10-22 13:21:28 -0400

    Added Combined Mode (Not Complete)

---
+++ simulink_group32/untitled_ert_rtw/ert_main.c
@@ -0,0 +1,115 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'untitled'.
+ *
+ * Model version                  : 1.1
+ * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
+ * C/C++ source code generated on : Fri Oct 21 16:49:26 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "untitled.h"
+#include "untitled_private.h"
+#include "rtwtypes.h"
+#include "limits.h"
+#include "board.h"
+#include "mw_cmsis_rtos.h"
+#define UNUSED(x)                      x = x
+#define NAMELEN                        16
+
+/* Function prototype declaration*/
+void exitFcn(int sig);
+void *terminateTask(void *arg);
+void *baseRateTask(void *arg);
+void *subrateTask(void *arg);
+volatile boolean_T stopRequested = false;
+volatile boolean_T runModel = true;
+mw_signal_event_t stopSem;
+mw_signal_event_t baserateTaskSem;
+mw_thread_t schedulerThread;
+mw_thread_t baseRateThread;
+void *threadJoinStatus;
+int terminatingmodel = 0;
+void *baseRateTask(void *arg)
+{
+  runModel = (rtmGetErrorStatus(untitled_M) == (NULL));
+  while (runModel) {
+    mw_osSignalEventWaitEver(&baserateTaskSem);
+    untitled_step();
+
+    /* Get model outputs here */
+    stopRequested = !((rtmGetErrorStatus(untitled_M) == (NULL)));
+  }
+
+  runModel = 0;
+  terminateTask(arg);
+  mw_osThreadExit((void *)0);
+  return NULL;
+}
+
+void exitFcn(int sig)
+{
+  UNUSED(sig);
+  rtmSetErrorStatus(untitled_M, "stopping the model");
+  runModel = 0;
+}
+
+void *terminateTask(void *arg)
+{
+  UNUSED(arg);
+  terminatingmodel = 1;
+
+  {
+    runModel = 0;
+  }
+
+  /* Terminate model */
+  untitled_terminate();
+  mw_osSignalEventRelease(&stopSem);
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+  SystemCoreClockUpdate();
+  hardware_init();
+  rtmSetErrorStatus(untitled_M, 0);
+
+  /* Initialize model */
+  untitled_initialize();
+
+  /* Call RTOS Initialization function */
+  mw_RTOSInit(0.001, 0);
+
+  /* Wait for stop semaphore */
+  mw_osSignalEventWaitEver(&stopSem);
+
+#if (MW_NUMBER_TIMER_DRIVEN_TASKS > 0)
+
+  {
+    int i;
+    for (i=0; i < MW_NUMBER_TIMER_DRIVEN_TASKS; i++) {
+      CHECK_STATUS(mw_osSignalEventDelete(&timerTaskSem[i]), 0,
+                   "mw_osSignalEventDelete");
+    }
+  }
+
+#endif
+
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
